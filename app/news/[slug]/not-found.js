dbb119ff98417ae0faac19e4adb8e188dff193d1
Deepu Cyriac <deepucvk123@gmail.com>
2024-07-27 16:31:08 +0530

    Added not found page for news & home page

---
+++ app/news/[slug]/not-found.js
@@ -0,0 +1,8 @@
+export default function NewsNotFoundPage() {
+  return (
+    <div id="error">
+      <h1>Not Found!</h1>
+      <p>Unfortunately, we couldn't find the requested news article!</p>
+    </div>
+  );
+}
