1c1c8a689cefc003a94215a016be586fd9e76e86
Deepu Cyriac <deepucvk123@gmail.com>
2024-07-30 08:18:16 +0530

    Moved db into project folder for direct fetching without be and added loading indicator

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,16 +1,22 @@
 import NewsList from "@/components/news-list";
+import { getAllNews } from "@/lib/news";
 
 //this is converted to a server component from a client component. check git or file history to view client side data fetching
 //this is the standard way to fetch data in next.js i.e, to fetch data using server component
 //it also provides seo features as the prepared data is returned from the server with the page
 export default async function NewsPage() {
-  const response = await fetch("http://localhost:8080/news");
+  //the below commented code is for server side data fetching
+  // const response = await fetch("http://localhost:8080/news");
 
-  if (!response.ok) {
-    throw new Error("Failed to fetch news.");
-  }
+  // if (!response.ok) {
+  //   throw new Error("Failed to fetch news.");
+  // }
 
-  const news = await response.json();
+  // const news = await response.json();
+
+  //the below code is for if your db is handled by you and you own the db, move it into project folder and direct fetch from db without backend
+
+  const news = await getAllNews();
 
   return (
     <>
